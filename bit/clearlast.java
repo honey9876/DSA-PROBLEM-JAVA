dbb4ca70aa814528664801d3917de53bba6078ad
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-09-27 11:44:48 +0530

    cleartlastbit

--- bit/clearlast.java
+++ bit/clearlast.java
@@ -1,5 +1,16 @@
 package bit;
-
 public class clearlast {
-    
+    public static int clearIBit(int n, int i){
+        int bitMask = (~0)<<i;
+        return n & bitMask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(clearIBit(15, 2));
+        
+    }
 }
+
+
+
+
