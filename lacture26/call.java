0f4786aacbae83a7f64d8828d200fa888b2aaf55
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-10-07 10:52:09 +0530

    call stack

---
+++ lacture26/call.java
@@ -0,0 +1,14 @@
+// package lacture26;
+
+// public class call {
+//     public static void printDec(int n) {
+//         if (n == 1) {
+//             System.out.println(1);
+//             return;
+            
+//         }
+//         System.out.println(n);
+//         return;
+//     }
+
+// }
