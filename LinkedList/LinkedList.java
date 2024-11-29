8903ace07ec4c8d1ed6aeb70bd462769416a9d69
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-11-29 12:32:19 +0530

    update code

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -14,6 +14,7 @@ public class LinkedList {
 
     private Node head; 
     private Node tail; 
+    // public static Node size;
 
 
     public LinkedList() {
@@ -60,21 +61,74 @@ public class LinkedList {
         }
         Node temp = head;
         while (temp != null){
-            System.out.println(temp.data+ " ");
+            System.out.print(temp.data+ " ");
             temp = temp.next;
         }
         System.out.println();
     }
+
+    public void add(int idx, int data){
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while( i < idx-1) {
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst(){
+        // if (size == 0) {
+        //     System.out.println("ll is empty");
+        // }
+        int val = head.data;
+        head = head.next;
+        return val;
+
+    }
+    public int itrSearch(int key){
+        Node temp = head;
+        int i = 0;
+
+        while(temp != null){
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+
+
     
-
-
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+        // ll.print();
         ll.addFirst(2); 
+        // ll.print();
         ll.addFirst(1);
+        // ll.print();
         ll.addLast(3);  
+        // ll.print();
         ll.addLast(4);  
+        ll.add(2, 3);
+        // ll.print();
 
-        ll.printList(); 
+        // ll.printList(); 
+        // ll.removeFirst();
+        ll.print();
+        System.out.println(ll.itrSearch(3));
+        System.out.println(ll.itrSearch(10));
+
     }
 }
